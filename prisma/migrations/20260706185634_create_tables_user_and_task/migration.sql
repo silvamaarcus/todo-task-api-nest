73a82c0092db0035e9755f82e151bbb446c86e11
Marcus Silva <devsilvamarcus@gmail.com>
2026-07-06 16:01:21 -0300

    feat: create initial migration for User and Task tables with relationships

---
+++ prisma/migrations/20260706185634_create_tables_user_and_task/migration.sql
@@ -0,0 +1,32 @@
+-- CreateEnum
+CREATE TYPE "TaskType" AS ENUM ('TODO', 'IN_PROGRESS', 'DONE');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "first_name" VARCHAR(50) NOT NULL,
+    "last_name" VARCHAR(50) NOT NULL,
+    "email" VARCHAR(100) NOT NULL,
+    "password" VARCHAR(100) NOT NULL,
+    "created_at" DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
+    "title" VARCHAR(200) NOT NULL,
+    "description" VARCHAR(500),
+    "status" "TaskType" NOT NULL DEFAULT 'TODO',
+    "created_at" DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
