fb18468008cd5852c0166c81c4f6c4749c98c334
Marcus Silva <devsilvamarcus@gmail.com>
2026-07-06 22:09:54 -0300

    feat: add ListTasksUseCase for retrieving tasks by user ID

---
+++ src/domain/use-cases/tasks/list-tasks.use-case.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { TaskType } from '@prisma/client';
+
+import type { ITaskRepository } from '@/domain/repositories/task.repository';
+
+export interface ListTasksUseCaseInput {
+  user_id: string;
+}
+
+export interface ListTasksUseCaseOutput {
+  tasks: {
+    id: string;
+    title: string;
+    description?: string | undefined;
+    status: TaskType;
+    user_id: string;
+  }[];
+}
+
+@Injectable()
+export class ListTasksUseCase {
+  constructor(private readonly taskRepository: ITaskRepository) {}
+
+  async execute(
+    userId: ListTasksUseCaseInput,
+  ): Promise<ListTasksUseCaseOutput> {
+    const tasks = await this.taskRepository.findAllByUserId(userId.user_id);
+
+    // Retorna a lista 'tasks' com os campos desejados. Aplica o map p/ transformar cada task no formato esperado.
+    return {
+      tasks: tasks.map((task) => ({
+        id: task.id,
+        title: task.title,
+        description: task.description || undefined,
+        status: task.status,
+        user_id: task.user_id,
+      })),
+    };
+  }
+}
