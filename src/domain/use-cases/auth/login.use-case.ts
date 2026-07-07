19bd573b8525bf2fa4ce7cfef7f5bb029d36937b
Marcus Silva <devsilvamarcus@gmail.com>
2026-07-07 09:58:34 -0300

    feat: enhance LoginUseCase to include JWT token generation

--- src/domain/use-cases/auth/login.use-case.ts
+++ src/domain/use-cases/auth/login.use-case.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 
 import type { IUserRepository } from '@/domain/repositories/user.repository';
@@ -15,8 +16,10 @@ export interface LoginOutput {
 @Injectable()
 export class LoginUseCase {
   constructor(
-    @Inject('IUserRepository') private readonly userRepository: IUserRepository,
-  ) {} // Injeção de dependência Repository, usando o token 'IUserRepository'
+    @Inject('IUserRepository') // Injeção de dependência Repository, usando o token 'IUserRepository'
+    private readonly userRepository: IUserRepository,
+    private readonly jwtService: JwtService, // Injeção de dependência JwtService, fornecido pelo módulo JwtModule do NestJS
+  ) {}
 
   async execute(loginUserParams: LoginInput): Promise<LoginOutput> {
     const user = await this.userRepository.findByEmail(loginUserParams.email); // Verifica se o usuário existe na db
@@ -34,6 +37,11 @@ export class LoginUseCase {
       throw new UnauthorizedException('Invalid credentials');
     }
 
-    return { accessToken: '' };
+    const accessToken = this.jwtService.sign({
+      sub: user.id,
+      email: user.email,
+    }); // Gera um token JWT com o id e email do usuário
+
+    return { accessToken };
   }
 }
