4a3b0b2ef1919e4b84c7999b34ad352eced2a111
Marcus Silva <devsilvamarcus@gmail.com>
2026-07-09 19:06:46 -0300

    docs: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
@@ -12,6 +13,16 @@ async function bootstrap() {
       forbidNonWhitelisted: true, // lança erro se vier algum campo não esperado
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Todo Task API')
+    .setDescription('API para gerenciamento de tarefas')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
